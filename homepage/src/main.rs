e187e56ab8b9a83da4dc2c204c2e063fb7b85d02
Khue Doan <khuedoan98@gmail.com>
2020-06-22 00:27:57 +0700

    Add homepage

---
+++ homepage/src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
+}
