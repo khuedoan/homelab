e3d39afb94c845c20e0162950800be4318d848a2
Khue Doan <khuedoan98@gmail.com>
2020-07-20 13:55:27 +0700

    Add test container

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,2 +1,17 @@
 provider "lxd" {
 }
+
+resource "lxd_container" "test1" {
+  name      = "test1"
+  remote    = "images"
+  image     = "ubuntu/18.04"
+  ephemeral = false
+
+  config = {
+    "boot.autostart" = true
+  }
+
+  limits = {
+    cpu = 2
+  }
+}
