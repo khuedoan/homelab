95cd9c1437eb50cf9b3bfe2b199369d68261af9e
Khue Doan <khuedoan98@gmail.com>
2020-07-20 14:07:41 +0700

    Add network

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,21 @@
 provider "lxd" {
+  generate_client_certificates = true
+  accept_remote_certificate    = true
+}
+
+resource "lxd_network" "kubernetes_subnetwork" {
+  name = "kubernetes"
+
+  config = {
+    "ipv4.address" = "10.150.19.1/24"
+    "ipv4.nat"     = "true"
+  }
 }
 
 resource "lxd_container" "test1" {
-  name      = "test1"
-  remote    = "images"
-  image     = "ubuntu/18.04"
+  count     = 3
+  name      = "test-${count.index}"
+  image     = "images:ubuntu/18.04"
   ephemeral = false
 
   config = {
