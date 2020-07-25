0189946cc875c86aa16469fe8dc8844e497e72d8
Khue Doan <khuedoan98@gmail.com>
2020-07-26 00:54:51 +0700

    Use profile for containers

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "lxd" {
   accept_remote_certificate    = true
 }
 
-resource "lxd_network" "kubernetes_network" {
+resource "lxd_network" "kubernetes" {
   name = "kubernetes"
 
   config = {
@@ -12,18 +12,15 @@ resource "lxd_network" "kubernetes_network" {
   }
 }
 
-resource "lxd_container" "kubernetes_controllers" {
-  count     = 3
-  name      = "controller-${count.index}"
-  image     = "images:ubuntu/18.04"
-  ephemeral = false
+resource "lxd_profile" "kubernetes" {
+  name = "kubernetes"
 
   config = {
-    "boot.autostart" = true
-  }
-
-  limits = {
-    cpu = 2
+    "limits.cpu"           = 2
+    "linux.kernel_modules" = "ip_tables,ip6_tables,netlink_diag,nf_nat,overlay"
+    "raw.lxc"              = "lxc.apparmor.profile=unconfined\nlxc.cap.drop= \nlxc.cgroup.devices.allow=a\nlxc.mount.auto=proc:rw\nsys:rw"
+    "security.privileged"  = "true"
+    "security.nesting"     = "true"
   }
 
   device {
@@ -32,17 +29,27 @@ resource "lxd_container" "kubernetes_controllers" {
 
     properties = {
       nictype = "bridged"
-      parent  = "${lxd_network.kubernetes_network.name}"
+      parent  = "lxdbr0"
     }
   }
 
-  provisioner "local-exec" {
-  command = <<EXEC
-    lxc exec ${self.name} -- bash -xe -c '
-      touch /hello
-    '
-    EXEC
+  device {
+    name = "root"
+    type = "disk"
+
+    properties = {
+      path = "/"
+      pool = "default"
+    }
   }
+}
+
+resource "lxd_container" "kubernetes_controllers" {
+  count     = 3
+  name      = "controller-${count.index}"
+  image     = "images:ubuntu/18.04"
+  ephemeral = false
+  profiles  = [ lxd_profile.kubernetes.name ]
 }
 
 resource "lxd_container" "kubernetes_workers" {
@@ -50,32 +57,7 @@ resource "lxd_container" "kubernetes_workers" {
   name      = "worker-${count.index}"
   image     = "images:ubuntu/18.04"
   ephemeral = false
-
-  config = {
-    "boot.autostart" = true
-  }
-
-  limits = {
-    cpu = 2
-  }
-
-  device {
-    name = "eth0"
-    type = "nic"
-
-    properties = {
-      nictype = "bridged"
-      parent  = "${lxd_network.kubernetes_network.name}"
-    }
-  }
-
-  provisioner "local-exec" {
-  command = <<EXEC
-    lxc exec ${self.name} -- bash -xe -c '
-      touch /hello
-    '
-    EXEC
-  }
+  profiles  = [ lxd_profile.kubernetes.name ]
 }
 
 resource "lxd_container" "haproxy" {
@@ -98,15 +80,7 @@ resource "lxd_container" "haproxy" {
 
     properties = {
       nictype = "bridged"
-      parent  = "${lxd_network.kubernetes_network.name}"
+      parent  = "${lxd_network.kubernetes.name}"
     }
-  }
-
-  provisioner "local-exec" {
-  command = <<EXEC
-    lxc exec ${self.name} -- bash -xe -c '
-      touch /hello
-    '
-    EXEC
   }
 }
