0c7393423e754a601f5c7843f13c353defc1e684
Khue Doan <khuedoan98@gmail.com>
2020-07-27 11:32:20 +0700

    Disable swap on host machine instead of LXD containers

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,6 @@ resource "lxd_profile" "kubernetes" {
 
   config = {
     "limits.cpu"           = 2
-    "limits.memory.swap"   = false
     "linux.kernel_modules" = "ip_tables,ip6_tables,netlink_diag,nf_nat,overlay"
     "raw.lxc"              = "lxc.apparmor.profile=unconfined\nlxc.cap.drop= \nlxc.cgroup.devices.allow=a\nlxc.mount.auto=proc:rw sys:rw"
     "security.privileged"  = true
