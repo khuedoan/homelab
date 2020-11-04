b5d1b02cdeeb9428f29663e7ed8fdf23b219fa06
Khue Doan <khuedoan98@gmail.com>
2020-11-05 01:06:15 +0700

    Use remote backend

--- platform/versions.tf
+++ platform/versions.tf
@@ -1,9 +1,19 @@
 terraform {
+  required_version = ">= 0.13"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "khuedoan"
+
+    workspaces {
+      name = "homelab"
+    }
+  }
+
   required_providers {
     rke = {
       source = "rancher/rke"
       version = "1.1.3"
     }
   }
-  required_version = ">= 0.13"
 }
