77162b83ea160b4e6d0446920f79bf597cc44bf7
Khue Doan <khuedoan98@gmail.com>
2020-11-04 23:37:04 +0700

    Change boot next before shutdown

--- infra/scripts/shutdown.sh
+++ infra/scripts/shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh root@node0.homelab.local poweroff
-ssh root@node1.homelab.local poweroff
-ssh root@node2.homelab.local poweroff
-ssh root@node3.homelab.local poweroff
+ssh root@node0.homelab.local 'efibootmgr --bootnext 0006 && poweroff'
+ssh root@node1.homelab.local 'efibootmgr --bootnext 0006 && poweroff'
+ssh root@node2.homelab.local 'efibootmgr --bootnext 0006 && poweroff'
+ssh root@node3.homelab.local 'efibootmgr --bootnext 0006 && poweroff'
