8569d8633eef51c9a17c851e4064a78747c8a61c
Khue Doan <khuedoan98@gmail.com>
2020-11-12 23:54:26 +0700

    Turn off PXE server when all server has started up

---
+++ infra/docker.sh
@@ -0,0 +1,4 @@
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl enable docker
