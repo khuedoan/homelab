aa3ffc8d9e297ac179dcc1014e6ab7aba86f02a1
Khue Doan <khuedoan98@gmail.com>
2020-11-21 01:42:35 +0700

    Install Vault

--- platform/main.tf
+++ platform/main.tf
@@ -116,3 +116,13 @@ resource "kubernetes_ingress" "grafana_ingress" {
      }
    }
  }
+
+resource "helm_release" "vault" {
+  name       = "vault"
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+  version    = "0.8.0"
+
+  namespace        = "vault"
+  create_namespace = true
+}
