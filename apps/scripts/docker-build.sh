f4eb269f9d043f7328da2f7ded9d8a6e97888c14
Khue Doan <khuedoan98@gmail.com>
2021-02-08 08:52:53 +0700

    Clean up

--- apps/scripts/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker build -t khuedoan/khuedoan.com-nginx nginx/
-docker build -t khuedoan/khuedoan.com-index index/
-docker build -t khuedoan/khuedoan.com-gitea gitea/
-docker build -t khuedoan/khuedoan.com-mariadb mariadb/
---
+++ docs/03_apps.md
@@ -0,0 +1,1 @@
+../apps/README.md
