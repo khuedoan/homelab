6d324360394a7404d8b4e99bcf4899cf2ce41be7
Khue Doan <khuedoan98@gmail.com>
2021-02-12 21:51:19 +0700

    Working lxd container

--- infra/lxd/terraform.tf
+++ infra/lxd/terraform.tf
@@ -1,6 +1,26 @@
 terraform {
   backend "etcdv3" {
-    endpoints = [ "localhost:2379" ]
+    endpoints = ["localhost:2379"]
     lock      = true
+  }
+
+  required_providers {
+    lxd = {
+      source  = "terraform-lxd/lxd"
+      version = "1.5.0"
+    }
+  }
+}
+
+provider "lxd" {
+  generate_client_certificates = true
+  accept_remote_certificate    = true
+
+  lxd_remote {
+    name     = "local"
+    scheme   = "https"
+    address  = "192.168.64.4"
+    password = "1"
+    default  = true
   }
 }
