69851a9d46e0f21b7d9943b98fa7dfd108bb9852
Khue Doan <khuedoan98@gmail.com>
2021-02-13 11:54:46 +0000

    Update lxd rke

--- infra/lxd/cluster.tf
+++ infra/lxd/cluster.tf
@@ -5,42 +5,35 @@ resource "lxd_container" "k8s" {
   ephemeral = false
 
   config = {
-    security.privileged  = true
-    security.nesting     = true
-    limits.memory.swap   = false
-    limits.cpus          = 1
-    linux.kernel_modules = "ip_tables,ip6_tables,nf_nat,overlay,br_netfilter"
-    raw.lxc       = <<-EOT
+    "security.privileged"  = true
+    "security.nesting"     = true
+    "linux.kernel_modules" = "ip_tables,ip6_tables,nf_nat,overlay,br_netfilter"
+    "raw.lxc"       = <<-EOT
       lxc.apparmor.profile=unconfined
       lxc.cap.drop=
       lxc.cgroup.devices.allow=a
       lxc.mount.auto=proc:rw sys:rw
     EOT
-    user.user-data       = <<-EOT
+    "user.user-data"       = <<-EOT
       #cloud-config
       ssh_authorized_keys:
         - ${file("~/.ssh/id_rsa.pub")}
       disable_root: false
+      packages:
+        - apt-transport-https
+        - ca-certificates
+        - curl
+        - gnupg-agent
+        - software-properties-common
       runcmd:
-        - apt-get update
-        - apt-get install -y iptables git telnet vim software-properties-common resolvconf linux-headers-$(uname -r)
-        - echo "nameserver 1.1.1.1" > /etc/resolvconf/resolv.conf.d/tail
-        - echo "RateLimitIntervalSec=0" >> /etc/systemd/journald.conf
-        - echo "RateLimitBurst=0" >> /etc/systemd/journald.conf
-        - systemctl restart systemd-journald.service
-        - systemctl start resolvconf
-        - /opt/rke2/run_rke2.sh
-      write_files:
-      - path: /opt/rke2/run_rke2.sh
-        permissions: "0755"
-        owner: root:root
-        content: |
-          #!/bin/bash
-          curl -fsSL https://raw.githubusercontent.com/rancher/rke2/master/install.sh --output install.sh
-          chmod u+x install.sh
-          INSTALL_RKE2_METHOD='tar' INSTALL_RKE2_TYPE=server INSTALL_RKE2_VERSION=v1.19.7+rke2r1 ./install.sh
-          systemctl enable rke2-server
-          systemctl start rke2-server
+        - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+        - add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        - apt-get update -y
+        - apt-get install -y docker-ce docker-ce-cli containerd.io
+        - mkdir -p /etc/systemd/system/docker.service.d/
+        - printf "[Service]\nMountFlags=shared" > /etc/systemd/system/docker.service.d/mount_flags.conf
+        - systemctl start docker
+        - systemctl enable docker
     EOT
   }
 
@@ -49,28 +42,38 @@ resource "lxd_container" "k8s" {
   }
 }
 
-# resource "rke_cluster" "cluster" {
-#   dynamic "nodes" {
-#     for_each = lxd_container.k8s
+resource "time_sleep" "wait_cloud_init" {
+  depends_on = [lxd_container.k8s]
 
-#     content {
-#       address = nodes.value.ip_address
-#       user    = "root"
-#       role = [
-#         "controlplane",
-#         "etcd",
-#         "worker"
-#       ]
-#       ssh_key = file("~/.ssh/id_rsa")
-#     }
-#   }
+  create_duration = "240s"
+}
 
-#   ingress {
-#     provider = "none"
-#   }
-# }
+resource "rke_cluster" "cluster" {
+  dynamic "nodes" {
+    for_each = lxd_container.k8s
 
-# resource "local_file" "kube_config_yaml" {
-#   filename = "${path.root}/kube_config.yaml"
-#   content  = rke_cluster.cluster.kube_config_yaml
-# }
+    content {
+      address = nodes.value.ip_address
+      user    = "root"
+      role = [
+        "controlplane",
+        "etcd",
+        "worker"
+      ]
+      ssh_key = file("~/.ssh/id_rsa")
+    }
+  }
+
+  ingress {
+    provider = "none"
+  }
+
+  ignore_docker_version = true
+
+  depends_on = [time_sleep.wait_cloud_init]
+}
+
+resource "local_file" "kube_config_yaml" {
+  filename = "${path.root}/kube_config.yaml"
+  content  = rke_cluster.cluster.kube_config_yaml
+}
