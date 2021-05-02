c1a993cd22f3135ad024b6c558f8f6c11330092a
Khue Doan <khuedoan98@gmail.com>
2021-05-02 21:30:26 +0700

    Restructure

--- infra/bootstrap.tf
+++
@@ -1,4 +0,0 @@
-module "rke_cluster_bootstrap" {
-  source      = "./modules/kubernetes-cluster-bootstrap"
-  kube_config = rke_cluster.cluster.kube_config_yaml
-}
--- infra/cluster.tf
+++ infra/cluster.tf
@@ -172,3 +172,8 @@ resource "local_file" "kube_config_yaml" {
   sensitive_content = rke_cluster.cluster.kube_config_yaml
   file_permission   = "0600"
 }
+
+module "rke_cluster_bootstrap" {
+  source      = "./modules/kubernetes-cluster-bootstrap"
+  kube_config = rke_cluster.cluster.kube_config_yaml
+}
