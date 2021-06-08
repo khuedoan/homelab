9b458435e09527a14a834205b1eebf96074cedc9
Khue Doan <khuedoan98@gmail.com>
2021-06-08 20:37:39 +0700

    Fix can't create tfstate container

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -1,9 +1,11 @@
 terraform {
   # TODO (feature) Generate endpoint automatically (terragrunt for variable)
-  #backend "etcdv3" {
-  #  endpoints = ["192.168.1.110:2379"]
-  #  lock      = true
-  #}
+  backend "etcdv3" {
+    endpoints = [
+      "192.168.1.110:23799"
+    ]
+    lock      = true
+  }
 
   required_providers {
     rke = {
