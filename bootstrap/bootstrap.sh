d6284215b35d87aac11c9edf489b56c9bb7a1c42
Khue Doan <khuedoan98@gmail.com>
2021-07-18 18:37:40 +0700

    Host network on dest sidero

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -50,3 +50,7 @@ clusterctl move \
   --kubeconfig-context=admin@talos-default \
   --to-kubeconfig=$HOME/.kube/config \
   --to-kubeconfig-context=admin@management-plane
+
+kubectl taint node talos-192-168-1-24 node-role.kubernetes.io/master:NoSchedule-
+
+kubectl patch deploy -n sidero-system sidero-controller-manager --type='json' -p='[{"op": "add", "path": "/spec/template/spec/hostNetwork", "value": true}]'
