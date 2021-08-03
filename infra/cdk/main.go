872aac3e92418c9b420772a11dc777c401d31b7d
Khue Doan <khuedoan98@gmail.com>
2021-08-03 17:14:13 +0000

    Initial Terraform CDK support

---
+++ infra/cdk/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/aws/constructs-go/constructs/v3"
+	"github.com/hashicorp/terraform-cdk-go/cdktf"
+)
+
+func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
+	stack := cdktf.NewTerraformStack(scope, &id)
+
+	// The code that defines your stack goes here
+
+	return stack
+}
+
+func main() {
+	app := cdktf.NewApp(nil)
+
+	NewMyStack(app, "cdk")
+
+	app.Synth()
+}
