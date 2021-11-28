71daf91eeffcae34257ab7b16d6c53c9d01be1cd
Khue Doan <khuedoan98@gmail.com>
2021-11-29 00:14:59 +0700

    docs: update provisioning flow diagram

--- docs/diagrams/provisioning_flow.py
+++ docs/diagrams/provisioning_flow.py
@@ -1,44 +1,38 @@
 from diagrams import Cluster, Diagram
 from diagrams.generic.os import LinuxGeneral
-from diagrams.k8s.ecosystem import Helm, Kustomize
+from diagrams.k8s.ecosystem import Helm
 from diagrams.k8s.infra import Node
-from diagrams.onprem.client import Client
-from diagrams.onprem.compute import Server
 from diagrams.onprem.container import Docker
 from diagrams.onprem.gitops import ArgoCD
-from diagrams.onprem.iac import Ansible, Terraform
 
 graph_attr = {
     "pad": "0"
 }
 
 with Diagram("Provisioning flow", graph_attr=graph_attr, outformat="jpg", show=False):
-    controller = Client("Initial controller")
-    bare_metal_machines = Server(f"Server(s)")
-
     with Cluster("./metal"):
-        ansible = Ansible("Ansible")
-        pxe = Docker("Ephemeral PXE server")
-        os = LinuxGeneral(f"Linux")
+        pxe = Docker("PXE server")
+        os = LinuxGeneral(f"Rocky Linux")
+        cluster = Node("K3s")
 
-        controller >> ansible
-        ansible >> pxe
-        pxe >> os
-        os >> bare_metal_machines
+        pxe >> os >> cluster
 
-        with Cluster("./cluster"):
-            terraform = Terraform("RKE")
-            kubernetes_nodes = Node("Kuberentes node(s)")
-            argocd = ArgoCD("ArgoCD")
+    with Cluster("./bootstrap"):
+        argocd = ArgoCD("ArgoCD")
 
-            controller >> terraform
-            terraform >> kubernetes_nodes
-            terraform >> argocd
+        cluster >> argocd
 
-            with Cluster("./apps"):
-                kustomize = Kustomize("Kustomize")
-                charts = Helm("Helm charts")
+    with Cluster("./system"):
+        system_charts = Helm("System charts")
 
-                controller >> kustomize
-                kustomize >> argocd
-                argocd >> charts
+        argocd >> system_charts
+
+    with Cluster("./platform"):
+        platform_charts = Helm("Platform charts")
+
+        argocd >> platform_charts
+
+    with Cluster("./apps"):
+        app_charts = Helm("Application charts")
+
+        argocd >> app_charts
