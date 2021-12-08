043329c5e65a65ad5ec428b107d5257cf40db835
Khue Doan <khuedoan98@gmail.com>
2021-12-08 10:32:20 +0700

    feat(external): add external ApplicationSet
    For charts that requires external resources

---
+++ external/versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "~> 1.0.0"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "khuedoan"
+
+    workspaces {
+      name = "homelab-external"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.4.0"
+    }
+
+    b2 = {
+      source  = "Backblaze/b2"
+      version = "~> 0.7.0"
+    }
+
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~> 2.7.0"
+    }
+  }
+}
