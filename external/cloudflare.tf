cfbf2304763bf528246713cc7dfc14b623d46294
Khue Doan <khuedoan98@gmail.com>
2021-12-24 13:27:53 +0700

    fix(external): temporarily disable IPv6 look up
    Unusable in Tekton task run
    
    Error: Error making request: Get "https://ipv6.icanhazip.com": dial tcp [REDACTED]:443: connect: network is unreachable

--- external/cloudflare.tf
+++ external/cloudflare.tf
@@ -12,14 +12,14 @@ data "http" "public_ipv4" {
   url = "https://ipv4.icanhazip.com"
 }
 
-data "http" "public_ipv6" {
-  url = "https://ipv6.icanhazip.com"
-}
+# data "http" "public_ipv6" {
+#   url = "https://ipv6.icanhazip.com"
+# }
 
 locals {
   public_ips = [
     "${chomp(data.http.public_ipv4.body)}/32",
-    "${chomp(data.http.public_ipv6.body)}/128"
+    # "${chomp(data.http.public_ipv6.body)}/128"
   ]
 }
 
