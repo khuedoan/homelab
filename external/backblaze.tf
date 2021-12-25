879355680060445b5113a9fd3ff8c11d8eb9eda9
Khue Doan <khuedoan98@gmail.com>
2021-12-25 21:58:02 +0700

    chore: cleanup Backblaze secret keys

--- external/backblaze.tf
+++ external/backblaze.tf
@@ -3,6 +3,11 @@ resource "b2_bucket" "backup" {
   bucket_type = "allPrivate"
 }
 
+resource "random_password" "backup_repo_password" {
+  length  = 64
+  special = false
+}
+
 resource "kubernetes_secret" "backblaze_credentials" {
   metadata {
     name      = "backblaze-credentials"
@@ -12,5 +17,6 @@ resource "kubernetes_secret" "backblaze_credentials" {
   data = {
     "application-key-id" = var.b2_application_key_id
     "application-key"    = var.b2_application_key
+    "repo-password"      = random_password.backup_repo_password.result
   }
 }
