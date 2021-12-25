675891c558c4e28ddacbfa5dc87690e5720e6272
Khue Doan <khuedoan98@gmail.com>
2021-12-25 20:18:45 +0700

    feat(external): inject Backblaze secrets to k8up namespace

--- external/backblaze.tf
+++ external/backblaze.tf
@@ -2,3 +2,15 @@ resource "b2_bucket" "backup" {
   bucket_name = "khuedoan-homelab-backup"
   bucket_type = "allPrivate"
 }
+
+resource "kubernetes_secret" "backblaze_credentials" {
+  metadata {
+    name      = "backblaze-credentials"
+    namespace = "k8up-operator"
+  }
+
+  data = {
+    "application-key-id" = var.b2_application_key_id
+    "application-key"    = var.b2_application_key
+  }
+}
