181265951f0950c702649cecf9afd5c628bd1a5d
Khue Doan <khuedoan98@gmail.com>
2021-12-27 18:23:58 +0700

    feat(bootstrap): use seed repo on first apply

---
+++ bootstrap/apply.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+curl -fks --connect-timeout 5 https://git.khuedoan.com \
+    || extra_args="--values values-seed.yaml"
+
+helm template \
+    --include-crds \
+    --namespace argocd \
+    ${extra_args} \
+    argocd . \
+
+# TODO https://github.com/argoproj/argo-helm/pull/1070
+# kubectl --namespace argocd wait --timeout=300s --for condition=ResourcesUpToDate \
+# 	applicationset/system \
+# 	applicationset/platform \
+# 	applicationset/apps
