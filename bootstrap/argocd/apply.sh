f4b63c018ff14694118a7e7d38428582b889affc
Khue Doan <khuedoan98@gmail.com>
2021-12-28 10:10:29 +0700

    refactor(bootstrap)!: split argocd and root

---
+++ bootstrap/argocd/apply.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+helm template \
+    --include-crds \
+    --namespace argocd \
+    argocd . \
+    | kubectl apply -n argocd -f -
+
+kubectl -n argocd wait --timeout=60s --for condition=Established \
+       crd/applications.argoproj.io \
+       crd/applicationsets.argoproj.io
