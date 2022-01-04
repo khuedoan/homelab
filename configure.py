8f70137b1d06e0a1db94388d95892af7d5e6797e
Khue Doan <mail@khuedoan.com>
2022-01-04 22:08:53 +0700

    build: initial support for configuration script

---
+++ configure.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+"""
+Basic configure script for new users
+"""
+
+import platform
+import sys
+
+if sys.version_info < (3, 10, 0):
+    raise Exception("Must be using Python >= 3.10.0")
+
+if platform.system() != 'Linux':
+    raise Exception("Only Linux is supported, please us a Linux VM or switch operating system")
+
+# TODO
+# - check if docker installed
+# - check if make installed
+# - confirm text editor $EDITOR
+# - change domain
+# - change seed repo
+# - change gitops repo
+# - add Gitea remote?
+# - change hardware info
+# - change Terraform workspace (and make it optional?)
+# - step counter?
