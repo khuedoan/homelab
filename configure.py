55c625559fddf1287b3f13f1ecbada665898540e
Khue Doan <mail@khuedoan.com>
2022-01-16 22:17:03 +0700

    build(script/configure): move default values to top

--- configure.py
+++ configure.py
@@ -11,6 +11,10 @@ import os
 import platform
 import sys
 
+editor = os.getenv('EDITOR')
+seed_repo = "github.com/khuedoan/homelab"
+domain = "khuedoan.com"
+
 if sys.version_info < (3, 10, 0):
     raise Exception("Must be using Python >= 3.10.0")
 
@@ -18,16 +22,13 @@ if platform.system() != 'Linux':
     raise Exception("Only Linux is supported, please us a Linux VM or switch operating system")
 
 # confirm text editor
-editor = os.getenv('EDITOR')
 editor = str(input(f"Text editor ({editor}): ") or editor)
 
 # Replace seed repo
-seed_repo = "github.com/khuedoan/homelab"
 seed_repo = str(input(f"Enter seed repo ({seed_repo}): ") or seed_repo)
 os.system(f"./scripts/replace-gitops-repo {seed_repo}")
 
 # Replace domain
-domain = "khuedoan.com"
 domain = str(input(f"Enter your domain ({domain}): ") or domain)
 os.system(f"./scripts/replace-domain {domain}")
 
