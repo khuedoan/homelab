1364a63f8f821366febbd907b722914e17efcc35
joeykleinsorge <josephkleinsorge@gmail.com>
2022-05-05 18:14:02 +0000

    feat: add tf

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "joeykleinsorge"
 
     workspaces {
       name = "homelab-external"
