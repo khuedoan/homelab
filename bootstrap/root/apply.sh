2115030a9060ed946e520e785ef1e2d78f23d511
Khue Doan <mail@khuedoan.com>
2022-07-21 01:58:00 +0700

    style: fix shellcheck

--- bootstrap/root/apply.sh
+++ bootstrap/root/apply.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+VALUES="values.yaml"
+
 curl -fks --connect-timeout 5 https://git.khuedoan.com \
-    || extra_args="--values values-seed.yaml"
+    || VALUES="values-seed.yaml"
 
 helm template \
     --include-crds \
     --namespace argocd \
-    ${extra_args} \
+    --values "${VALUES}" \
     argocd . \
     | kubectl apply -n argocd -f -
