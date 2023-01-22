cd39632439e0d57a1a0fcbed5cc7d80845e2498f
Khue Doan <mail@khuedoan.com>
2023-01-22 12:24:29 +0700

    fix(argocd): do not apply ServiceMonitor on bootstrap

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+VALUES="values.yaml"
+
+kubectl get ingress argocd-server --namespace argocd \
+    || VALUES="values-seed.yaml"
+
 helm template \
     --dependency-update \
     --include-crds \
     --namespace argocd \
+    --values "${VALUES}" \
     argocd . \
     | kubectl apply -n argocd -f -
 
