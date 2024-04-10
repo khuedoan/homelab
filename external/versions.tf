72ecb4b1d96e8d894411a04ba4a0288ed3078361
donydonald1 <donydonald1@icloud.com>
2024-04-10 01:55:40 -0500

    added config for audit

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "homelab-external"
+    organization = "Techsecom"
 
     workspaces {
       name = "homelab-external"
