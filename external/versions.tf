da14bd5858e929ae02894c3a8164b2657aa5a1ca
brimdor <chrisnelsonx@gmail.com>
2024-04-14 15:40:53 -0500

    Cleaned up config, added reset script
    - Config is cleaned up to use placeholders rather than khuedoan specific data to be more friendly for open source use
    - built a reset script that is now in the scripts folder to help clean out and restart the entire process if needed.

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "<terraform_workspace>"
 
     workspaces {
       name = "homelab-external"
