e6142394e04cc133e5e9bd4ef5cec5b57beb0cfd
Khue Doan <mail@khuedoan.com>
2025-01-18 11:34:59 +0700

    chore: upgrade dependencies

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Homelab";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
