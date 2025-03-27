526a4bbf8d490ce7521864a5a3981bfbb682b728
Khue Doan <mail@khuedoan.com>
2025-03-27 23:27:57 +0700

    refactor!: clean deps before rebuilding bootstrap

--- flake.nix
+++ flake.nix
@@ -1,59 +1,23 @@
 {
-  description = "Homelab";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          packages = [
-            ansible
-            ansible-lint
-            bmake
-            diffutils
-            docker
-            docker-compose
-            dyff
-            git
-            go
-            gotestsum
-            iproute2
-            jq
-            k9s
-            kanidm
-            kube3d
-            kubectl
-            kubernetes-helm
-            kustomize
-            libisoburn
-            neovim
-            openssh
-            opentofu # Drop-in replacement for Terraform
-            p7zip
-            pre-commit
-            qrencode
-            shellcheck
-            wireguard-tools
-            yamllint
-
-            (python3.withPackages (p: with p; [
-              jinja2
-              kubernetes
-              mkdocs-material
-              netaddr
-              pexpect
-              rich
-            ]))
-          ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs }:
+  let
+    supportedSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+    ];
+  in
+  {
+    devShells = supportedSystems (system: {
+      default = with nixpkgs.legacyPackages.${system}; mkShell {
+        packages = [
+          go
+          kubectl
+          opentofu
+        ];
+      };
+    });
+  };
 }
